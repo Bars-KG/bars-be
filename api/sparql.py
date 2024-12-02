ce3dc2834f0aed22235e5b467e673c9e873838fd
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-12-02 17:47:02 +0700

    feat: update search recommendation query

--- api/sparql.py
+++ api/sparql.py
@@ -1,6 +1,6 @@
 LOCAL_SEARCH_AIRPORTS_RECOMMENDATION = r"""
 SELECT ?airport ?label ?code WHERE {{
-    ?airport rdf:type :Airport;
+    ?airport rdf:type :Aerodrome;
     	rdfs:label ?label ;
         verb:gpsCode ?code .
     
@@ -13,7 +13,7 @@ REMOTE_SEARCH_AIRPORTS_RECOMMENDATION = r"""
 SELECT ?airport_code ?description
 WHERE {{
   VALUES ?airport_code {{ {codes} }}
-  ?airport wdt:P31 wd:Q1248784 ;
+  ?airport wdt:P31 wd:Q62447 ;
            wdt:P239 ?airport_code ;
            schema:description ?description .
   FILTER(LANG(?description) = "en")
